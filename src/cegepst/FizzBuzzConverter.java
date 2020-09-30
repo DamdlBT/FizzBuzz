209e4f8b631264a922e64a1f7f9cd7b33b75280f
DamdlBT <56141589+DamdlBT@users.noreply.github.com>
2020-09-30 18:02:08 -0400

    Ajout Calcul FizzBuzz

--- src/cegepst/FizzBuzzConverter.java
+++ src/cegepst/FizzBuzzConverter.java
@@ -3,6 +3,15 @@ package cegepst;
 public class FizzBuzzConverter {
 
     public String convert(int number) {
-        return "";
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        return String.valueOf(number);
     }
 }
